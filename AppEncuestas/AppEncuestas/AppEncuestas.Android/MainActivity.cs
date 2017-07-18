c92c956ba33f0226970146cf6b07d0c3d65782aa
danieltabares <danitabares5@hotmail.com>
2017-07-18 09:26:46 -0500

    publish app

--- AppEncuestas/AppEncuestas/AppEncuestas.Android/MainActivity.cs
+++ AppEncuestas/AppEncuestas/AppEncuestas.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AppEncuestas.Droid
 {
-    [Activity(Label = "AppEncuestas", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AppEncuestas", Icon = "@drawable/encu", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
